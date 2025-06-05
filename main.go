4cd4b09df8f102ad1022119943860772c761d612
AumSahayata <aumsahayata9@gmail.com>
2025-06-05 16:10:30 +0530

    completed write funtion

--- main.go
+++ main.go
@@ -3,9 +3,121 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/jcelliott/lumber"
 )
 
 const Version = "1.0.1"
+
+type (
+	Logger interface {
+		Fatal (string, ...interface{})
+		Error (string, ...interface{})
+		Warn (string, ...interface{})
+		Info (string, ...interface{})
+		Debug (string, ...interface{})
+		Trace (string, ...interface{})
+	}
+
+	Driver struct {
+		mutex sync.Mutex
+		mutexes map[string]*sync.Mutex
+		dir string
+		log Logger
+	}
+)
+
+type Options struct {
+	Logger
+}
+
+func New(dir string, options Options) (*Driver, error) {
+	dir = filepath.Clean(dir)
+
+	opts := Options{}
+
+	if options != nil {
+		opts = *options
+	}
+
+	if opts.Logger == nil {
+		opts.Logger = lumber.NewConsoleLogger((lumber.INFO))
+	}
+
+	driver := Driver{
+		dir: dir,
+		mutexes: make(map[string]*sync.Mutex),
+		log: opts.Logger,
+	}
+
+	if _, err := os.Stat(dir); err == nil {
+		opts.Logger.Debug("Using '%s' (database already exists)\n", dir)
+		return &driver, nil
+	}
+
+	opts.Logger.Debug("Creating the database at '%s'...\n", dir)
+	return &driver, os.MkdirAll(dir, 0755)
+}
+
+func (d *Driver) Write(collection, resource string, v interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("Missing collection - no place to save record!")
+	}
+
+	if resource == "" {
+		return fmt.Errorf("Missing collection - usable to save record (no name)!")
+	}
+
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, collection)
+	fnlPath := filepath.Join(dir, resource + ".json")
+	tmpPath := fnlPath + ".tmp"
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	b, err := json.MarshalIndent(v, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	b = append(b, byte('\n'))
+
+	if err := os.WriteFile(tmpPath, b, 0644); err != nil {
+		return err
+	}
+}
+
+func (d *Driver) Read() error {
+
+}
+
+func (d *Driver) ReadAll() () {
+
+}
+
+func (d *Driver) Delete() error {
+
+}
+
+func (d *Driver) getOrCreateMutex() *sync.Mutex {
+
+}
+
+func stat(path string) (fi os.FileInfo, err error) {
+	if fi, err = os.Stat(path); os.IsNotExist(err) {
+		fi, err = os.Stat(path + ".json")
+	}
+	return  
+}
 
 type Address struct {
 	City string
